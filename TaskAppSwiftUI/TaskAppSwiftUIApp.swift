e189290169f5628e046e5889947875d05e739a63
Alif Ramadhoni <annur.alif@phincon.com>
2021-06-03 09:28:52 +0700

    Initial commit

---
+++ TaskAppSwiftUI/TaskAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskAppSwiftUIApp.swift
+//  TaskAppSwiftUI
+//
+//  Created by Alif on 03/06/21.
+//
+
+import SwiftUI
+
+@main
+struct TaskAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
